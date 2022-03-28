93c840e8ae637f3464087c4f8f0f8cce114a9b28
Jarda Korinek <korinek14.j@gmail.com>
2022-03-29 00:07:30 +0200

    made first version of controlled textarea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <Editor />;
+  }
+}
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <textarea
+          id="editor"
+          value={this.state.input}
+          onChange={this.handleChange}
+        ></textarea>
+        <Previewer input={this.state.input} />
+      </div>
+    );
+  }
+}
+
+class Previewer extends React.Component {
+  render() {
+    return (
+      <div id="preview">
+        <h1>{this.props.input}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
